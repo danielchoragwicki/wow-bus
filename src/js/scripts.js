883e05a978ab6695ef3dabc59643490a776caef5
Daniel Chorągwicki <choragwickid@gmail.com>
2018-07-12 14:16:15 +0200

    Full responsive inputs, add jquery-ui and scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,58 @@
 $(document).ready(function () {
     
+    // CITIES
+
+    let fromCity = [
+        'Wrocław',
+        'Kraków',
+        'Warszawa',
+        'Berlin'
+    ];
+    let destCity = [
+        'Wrocław',
+        'Kraków',
+        'Warszawa',
+        'Berlin'
+    ];
+
+    $( "#from-city" ).autocomplete({
+        source: fromCity
+    });
+    $( "#dest-city" ).autocomplete({
+        source: destCity
+    });
+
+    // DATES
+
+    $(function() {
+        $(".datepicker").datepicker({
+            showOn: "button",
+            buttonImage: "../img/search-calendar.png",
+            buttonImageOnly: true,
+            buttonText: "Wybierz datę"
+        });
+    });
+
+    $(".search__add-button").click(function() {
+        $(".return").removeClass('hide');
+        $(".return-button").addClass('hide');
+    })
+
+    // PERSONS 
+
+    $("#persons-count-slider").slider({
+        range: "max",
+        min: 1,
+        max: 10,
+        value: 2,
+        slide: function(event, ui) {
+        $( "#persons-count" ).val(ui.value);
+        }
+    });
+    $("#persons-count").val($("#persons-count-slider").slider("value"));
+
+    $(".search__persons-button").click(function() {
+        $(".search__slider").toggleClass('hide');
+    })
+
 });
